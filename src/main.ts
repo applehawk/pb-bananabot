a3cf85994edb5b86888919196f37997c53f67c44
Vlad <mr.vasilenko.vlad@ya.ru>
2024-02-19 23:23:14 +0300

    Change listening port and container port to 8080

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   };
  
   const app = await NestFactory.create(AppModule, { httpsOptions } );
-  await app.listen(3000);
+  await app.listen(8080);
 
 }
 bootstrap();
