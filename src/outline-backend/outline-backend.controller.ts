754a39101156d7fab87fccb8028aded03aa7b005
Vlad <mr.vasilenko.vlad@ya.ru>
2024-02-20 15:53:19 +0300

    Fix port / serverAddress parse

--- src/outline-backend/outline-backend.controller.ts
+++ src/outline-backend/outline-backend.controller.ts
@@ -97,11 +97,11 @@ export class OutlineBackendController {
         let response = await this.createNewKey(telegramId)
 
         let regexKey = /ss:\/\/(\w+)@(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}):(\w+)\//g
-        let [ encodedBase64Key, serverAddress, port ] = [...response.accessUrl.matchAll(regexKey)][0]
+        let [ regexKeyMatch, encodedBase64Key, serverAddress, port ] = [...response.accessUrl.matchAll(regexKey)][0]
         console.log(`url64code: ${encodedBase64Key}, serverAddress: ${serverAddress}, port: ${port}`)
         let decodedBase64Key: string = atob(encodedBase64Key);
         let regexBase64Key = /(.+):(.+)/g
-        let [ encrypt_method, password ] = [...decodedBase64Key.matchAll(regexBase64Key)][0]
+        let [ regexBase64KeyMatch, encrypt_method, password ] = [...decodedBase64Key.matchAll(regexBase64Key)][0]
         
         let newConnection = await this.connectionService.createConnection({
             tgid: telegramId,
