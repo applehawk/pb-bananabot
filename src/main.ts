e9f7db000e1e3d52a3da1a34b18b557f56b792b0
Vlad <mr.vasilenko.vlad@ya.ru>
2024-02-20 14:18:08 +0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 async function bootstrap() { 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: "*", allowedHeaders:"*", methods: "*", credentials: true });
   await app.init();
-
   await app.listen(process.env.PORT || 80);
 }
 bootstrap();
