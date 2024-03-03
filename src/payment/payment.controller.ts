fea0e6dda4ff66b186d9f61c85064d4733c52750
Vlad Skipper <ivlad.skipper@gmail.com>
2024-03-03 16:00:53 +0300

    remove .env and change success at payment.controller

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Header, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Header, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { PaymentService } from './payment.service';
 
 import { ConfigService } from '@nestjs/config';
@@ -7,29 +8,34 @@ import { YooMoneyNotification } from '@app/yoomoney-client/types/notification.ty
 
 @Controller('/payment')
 export class PaymentController {
+    botUrl?: string
+
     constructor(private readonly paymentService: PaymentService,
         private readonly yooMoney: YooMoneyClient,
-        private readonly configService: ConfigService) {}
+        private readonly configService: ConfigService) {
+            this.botUrl = configService.get("BOT_URL");
+        }
 
     @Get('yoomoney/success')
-    success(@Res() res: any) {
-    return { data: 'success' };
+    success(@Res() res: Response) {
+        return res.redirect(this.botUrl);
+        //return { data: 'success' };
     }
 
     @Post('/yoomoney/notification')
     async yooMoneyWebHook(@Body() body: YooMoneyNotification) {
-    const isValid = await this.paymentService.yooMoneyWebHook(body);
+        const isValid = await this.paymentService.yooMoneyWebHook(body);
 
-    if (isValid) {
-        return { data: 'success' };
-    } else {
-        return { data: 'invalid' };
-    }
+        if (isValid) {
+            return { data: 'success' };
+        } else {
+            return { data: 'invalid' };
+        }
     }
 
     @Get(':paymentId')
     @Header('content-type', 'text/html')
     async getPayment(@Param('paymentId') paymentId: string): Promise<string> {
-    return this.paymentService.getPaymentForm(paymentId);
+        return this.paymentService.getPaymentForm(paymentId);
     }
 }
